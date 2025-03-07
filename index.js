784679e7d095f5860458beed5e47f18e9fe0e4d4
Daniel-sql123 <dadelgadoal@est.utn.ac.cr>
2025-03-06 18:12:32 -0600

    Se crean la barra de inicio y la imagen
    Se logra crear la barra de inicio con boostrap y se pone una imagen de bienvenida

---
+++ index.js
@@ -0,0 +1,5 @@
+function toggleDropdown() {
+    var dropdownContent = document.querySelector('.dropdown-content');
+    // Alternar la visibilidad del menú
+    dropdownContent.style.display = (dropdownContent.style.display === 'block') ? 'none' : 'block';
+}
