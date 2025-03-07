5067aedf0cdca20540cdec97657d94a70e475e82
Daniel-sql123 <dadelgadoal@est.utn.ac.cr>
2025-03-06 18:20:37 -0600

    Se borra una carpeta

--- index.js
+++
@@ -1,5 +0,0 @@
-function toggleDropdown() {
-    var dropdownContent = document.querySelector('.dropdown-content');
-    // Alternar la visibilidad del menú
-    dropdownContent.style.display = (dropdownContent.style.display === 'block') ? 'none' : 'block';
-}
